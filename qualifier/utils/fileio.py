e7c9cc3bb9d0926ebffc80a34b77d750032f9c2e
Billy <billy.bishop@rice.edu>
2021-07-05 15:25:08 -0500

    committing initial code

---
+++ qualifier/utils/fileio.py
@@ -0,0 +1,45 @@
+"""Helper functions to load and save CSV data.
+
+This contains a helper function for loading and saving CSV files.
+
+"""
+import csv
+
+
+def load_csv(csvpath):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        next(csvreader)
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+def save_csv(csvpath, data, header=None):
+    """Saves the CSV file to path provided
+    
+    Args:
+        csvpath: the csv file path
+        data: list of rows
+        header: optional header for csv file
+
+    """
+    with open(csvpath, "w", newline="") as csvfile:
+       csvwriter = csv.writer(csvfile, delimiter=",")
+       if header:
+           csvwriter.writerow(header)
+    csvwriter.writerows(data)
+
